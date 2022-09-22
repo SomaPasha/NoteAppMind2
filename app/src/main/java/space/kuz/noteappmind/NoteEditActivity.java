b57f19e84b3809219275ebfa9dde93fa6a75d7f8
Ferpasha <pavel.kuzneczov.96@gmail.com>
2022-09-22 22:23:50 +0500

    2 активити

---
+++ app/src/main/java/space/kuz/noteappmind/NoteEditActivity.java
@@ -0,0 +1,14 @@
+package space.kuz.noteappmind;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoteEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

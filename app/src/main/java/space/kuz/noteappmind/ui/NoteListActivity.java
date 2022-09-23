2c3553c86beac7eeb4df6e36c0591e1f99083b80
Ferpasha <pavel.kuzneczov.96@gmail.com>
2022-09-23 21:45:01 +0500

    repo

--- app/src/main/java/space/kuz/noteappmind/ui/NoteListActivity.java
+++ app/src/main/java/space/kuz/noteappmind/ui/NoteListActivity.java
@@ -1,11 +1,9 @@
 package space.kuz.noteappmind.ui;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;

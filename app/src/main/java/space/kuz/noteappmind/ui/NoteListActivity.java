2544f147503eea16b7607ffed919fcc8bce1c04e
Ferpasha <pavel.kuzneczov.96@gmail.com>
2022-09-23 21:43:50 +0500

    repo

--- app/src/main/java/space/kuz/noteappmind/ui/NoteListActivity.java
+++ app/src/main/java/space/kuz/noteappmind/ui/NoteListActivity.java
@@ -1,6 +1,7 @@
 package space.kuz.noteappmind.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,9 +12,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import space.kuz.noteappmind.R;
+import space.kuz.noteappmind.domain.NoteEntity;
+import space.kuz.noteappmind.domain.NotesRepo;
+import space.kuz.noteappmind.domain.NotesRepoImp;
 
 public class NoteListActivity extends AppCompatActivity {
     private Toolbar toolbar ;
+
+    private NotesRepo notesRepo = new NotesRepoImp();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +31,11 @@ public class NoteListActivity extends AppCompatActivity {
     public void initToolBar(){
         toolbar = findViewById(R.id.note_list_toolbar);
         setSupportActionBar(toolbar);
+        fillRepoByTestValue();
 
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,7 +47,7 @@ public class NoteListActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.new_note_menu:{
-                Toast.makeText(this,"ssss",Toast.LENGTH_LONG).show();
+              openNoteScreen();
                 return true;
             }
             default:{
@@ -45,5 +55,23 @@ public class NoteListActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    public void openNoteScreen(){
+        Intent intent = new Intent(this, NoteEditActivity.class);
+        startActivity(intent);
+    }
+
+    private void fillRepoByTestValue() {
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
+        notesRepo.addNote(new NoteEntity("dsdfdsfv","sdfsdfsdfwdfsddfsdfsd"));
     }
 }
